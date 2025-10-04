16c739c57e59bdcc53df965e068e6b6053695c74
newjasil <jasiljasil002@gmail.com>
2025-10-04 23:27:38 +0530

    config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @mjteachbot
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@mjteachbot
+# Ask Doubt on telegram @mjteachbot
 
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = int(environ.get("API_ID", "26590441"))
+    API_HASH = environ.get("API_HASH", "dd266d67accd373e2e746ba74af307c4")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
+    BOT_SESSION = environ.get("BOT_SESSION", "jasilbot") 
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://mhdishaqop333_db_user:<3CKE0RPbGUT3pk7f>@cluster0.9maeipj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "jasil-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", "8017823865"))
 
-# Don't Remove Credit Tg - @VJ_Botz
+# Don't Remove Credit Tg - @mjteachbot
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Ask Doubt on telegram @mjteachbot
 
 class temp(object): 
     lock = {}
@@ -24,6 +24,6 @@ class temp(object):
     BANNED_USERS = []
     IS_FRWD_CHAT = []
 
-# Don't Remove Credit Tg - @VJ_Botz
+# Don't Remove Credit Tg - @mjteachbot
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Ask Doubt on telegram @mjteachbot
